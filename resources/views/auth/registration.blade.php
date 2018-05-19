1e7c055de3cd4335d803050e10ce7a378d4e3fdb
kusamops <katy.bas@mail.ru>
2018-05-19 22:12:43 +0300

    change register form

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -5,35 +5,63 @@
 @endsection
 
 @section('content')
-	<form action="{{ route('register') }}" method="POST">
-		{{ csrf_field() }}
+	<div class="container">
+	
+		<form action="{{ route('register') }}" method="POST" class="form-horizontal">
+			{{ csrf_field() }}
 
-		<p>Registration</p>
+			<h2>Registration</h2>
 
-		<label for="">Name</label>
-		<input type="text" name="name">
+			<div class="form-group">
+				@include('layout.errors')
+			</div>
+			
+			<div class="form-group">
+				<label>Name</label>
+				<input type="text" name="name" class="form-control col-sm-5">
+			</div>
 
-		<label for="">Address</label>
-		<input type="text" name="address">
+			<div class="form-group">
+				<label for="">Address</label>
+				<input type="text" name="address" class="form-control col-sm-5">
+			</div>
 
-		<label for="">Phone</label>
-		<input type="text" name="phone">
+			<div class="form-group">
+				<label for="">Phone</label>
+				<input type="text" name="phone" class="form-control col-sm-5">
+			</div>
 
-		<label for="">Email</label>
-		<input type="text" name="email">
+			<div class="form-group">
+				<label for="">Email</label>
+				<input type="text" name="email" class="form-control col-sm-5">
+			</div>
 
-		<label for="">password</label>
-		<input type="password" name="password">
+			<div class="form-group">
+				<label for="">password</label>
+				<input type="password" name="password" class="form-control col-sm-5">
+			</div>
 
-		<label for="">Confirm your password</label>
-		<input type="password" name="password_confirmation">
-		
-		<label for="isteacher">Teacher</label>
-		<input type="radio" id="isteacher" name="status">
+			<div class="form-group">
+				<label for="">Confirm your password</label>
+				<input type="password" name="password_confirmation" class="form-control col-sm-5">
+			</div>
 
-		<label for="isstudent">Student</label>
-		<input type="radio" id="isstudent" name="status" checked>
+			<div class="form-group row">
+				<label for="isteacher" class="col-sm-2">Teacher</label>
+				<input type="radio" id="isteacher" name="status" class="form-control col-sm-2">
+			</div>
 
-		<button type="submit">Submit</button>
-	</form>
+			<div class="form-group row">
+				<label for="isstudent" class="col-sm-2">Student</label>
+				<input type="radio" id="isstudent" name="status" class="form-control col-sm-2" checked>
+			</div>
+
+			<div class="form-group">
+				
+			<button type="submit" class="btn btn-default " class="form-control col-sm-5">Submit</button>
+			</div>
+		</form>
+
+	</div>
+
 @endsection
--- resources/views/auth/signin.blade.php
+++ resources/views/auth/signin.blade.php
@@ -5,16 +5,31 @@
 @endsection
 
 @section('content')
+<div class="container">
+	
 	<form action="{{ route('login') }}" method="POST">
 		{{ csrf_field() }}
 
-		<p>Signing in</p>
-		<label for="">Email</label>
-		<input type="text" name="email">
+		<h2>Signing in</h2>
+		
+		<div class="form-group">
+			@include('layout.errors')
+		</div>
 
-		<label for="">password</label>
-		<input type="password" name="password">
+		<div class="form-group">
+			<label for="email">Email Adress:</label>
+			<input type="email" class="form-control" id="email" name="email" required>	
+		</div>
 
-		<button type="submit">Submit</button>
+		<div class="form-group">
+			<label for="password">Password:</label>
+			<input type="password" class="form-control" id="password" name="password" required>	
+		</div>
+
+		<div class="form-group">
+			<button type="submit" class="btn btn-default">Sign in</button>
+		</div>
 	</form>
+
+</div>
 @endsection
